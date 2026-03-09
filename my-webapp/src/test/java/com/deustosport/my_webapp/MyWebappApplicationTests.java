84b8f449e0ceaf38c54f1e8d5348e04346f96e61
Markel Bugallo Gonzalez <markel.bugallo@opendeusto.es>
2026-03-09 18:11:08 +0100

    Agregar configuración inicial de Gradle y estructura de proyecto Spring Boot

---
+++ my-webapp/src/test/java/com/deustosport/my_webapp/MyWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deustosport.my_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
